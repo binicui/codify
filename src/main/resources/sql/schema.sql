cb3bd4be610f7dc396234aa0729d4a0987158f17
bincuipark <tnqls3718@gmail.com>
2025-07-12 13:53:22 +0900

    최초 커밋

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,43 @@
+
+
+
+-- 키워드
+-- INT VARCHAR() TINYINT() CHAR() DATETIME
+-- NOT NULL NULL AUTO_INCREMENT COMMENT UNIQUE
+-- PRIMARY KEY (), FOREIGN KEY () REFERENCES tablename()
+
+-- DROP TABLE IF EXISTS tablename CASCADE;
+-- CREATE TABLE IF NOT EXISTS tablename COMMENT '' (
+-- );
+
+
+-- ######################################################################
+--  USER
+-- ######################################################################
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE IF NOT EXISTS tablename COMMENT '' (
+);
+
+DROP TABLE IF EXISTS user_roles CASCADE;
+CREATE TABLE IF NOT EXISTS tablename COMMENT '' (
+);
+
+DROP TABLE IF EXISTS user_profiles CASCADE;
+CREATE TABLE IF NOT EXISTS tablename COMMENT '' (
+);
+
+DROP TABLE IF EXISTS tablename CASCADE;
+CREATE TABLE IF NOT EXISTS tablename COMMENT '' (
+);
+
+DROP TABLE IF EXISTS tablename CASCADE;
+CREATE TABLE IF NOT EXISTS tablename COMMENT '' (
+);
+
+DROP TABLE IF EXISTS tablename CASCADE;
+CREATE TABLE IF NOT EXISTS tablename COMMENT '' (
+);
+
+
+
+
